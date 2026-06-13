300acb0462a67833a86fc2ce6c2bc8dcfe525ca1
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-06-13 20:37:43 -0300

    add constructor

--- src/service/Servicios.java
+++ src/service/Servicios.java
@@ -1,15 +1,30 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import model.Camion;
 import model.Paquete;
 
-public class Servicio {
+public class Servicios {
     //Completar con las estructuras y métodos privados que se requieran.
+    //estructuras clases
+    private List<Camion> camiones;
+    private List<Paquete> paquetes;
+    //estructura servicio 1
+    private HashMap<String, Paquete> paquetesPorCodigo;
+
+
     /*
     * Expresar la complejidad temporal del constructor.
+    *  ?????????
     */
-    public Servicios(String pathCamiones, String pathPaquetes){ }
+    public Servicios(String pathCamiones, String pathPaquetes){
+        this.camiones = new ArrayList<>();
+        this.paquetes = new ArrayList<>();
+        this.paquetesPorCodigo = new HashMap<>();
+    }
     /*
     * Expresar la complejidad temporal del servicio 1.
     */
