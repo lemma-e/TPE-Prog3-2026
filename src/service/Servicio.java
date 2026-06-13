2c3368100665b153c97fe29daa24451ce6f9cfb3
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-06-13 20:24:09 -0300

    add class Servicio

---
+++ src/service/Servicio.java
@@ -0,0 +1,25 @@
+package service;
+
+import java.util.List;
+
+import model.Paquete;
+
+public class Servicio {
+    //Completar con las estructuras y métodos privados que se requieran.
+    /*
+    * Expresar la complejidad temporal del constructor.
+    */
+    public Servicios(String pathCamiones, String pathPaquetes){ }
+    /*
+    * Expresar la complejidad temporal del servicio 1.
+    */
+    public Paquete servicio1(String codigoPaquete) { }
+    /*
+    * Expresar la complejidad temporal del servicio 2.
+    */
+    public List<Paquete> servicio2(boolean contieneAlimentos) { }
+    /*
+    * Expresar la complejidad temporal del servicio 3.
+    */
+    public List<Paquete> servicio3(int urgenciaMinima, int urgenciaMaxima) { }
+}
