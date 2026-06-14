5e07b6bbe898f7512a8594e8282a7f71a6b16d25
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-06-14 00:21:38 -0300

    add planteamiento backtraking

--- src/algorithm/Backtracking.java
+++ src/algorithm/Backtracking.java
@@ -2,4 +2,20 @@ package algorithm;
 
 public class Backtracking {
 
+
+     /*
+     * Raiz: no hay paquetes asignados
+     * Estados: cada estado es una asignacion parcial de paquetes a camiones
+     * Niveles: cada nivel representa la decision sobre un paquete
+     * Hijos: Asignar el paquete actual a cada camion posible
+     * Hoja: una hoja es solucion si ya se decidio sobre todos los paquetes
+     * Solucion: asignacion completa de paquetes a camiones
+     * Objetivo: minimizar el peso total de paquetes no asignados
+     * Podas:
+     * - !NO asignar si se supera la capacidad del camion
+     * - !NO asignar alimentos a un camion no refrigerado
+     * - Podar si el peso no asignado ya no mejora la mejor solucion.
+     */
+
+    private void back()
 }
