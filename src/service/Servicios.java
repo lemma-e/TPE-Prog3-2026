815c61ff74dfb86974312ec2bf6ec85f3acaea93
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-06-13 21:02:51 -0300

    Add partial feat servicio1

--- src/service/Servicios.java
+++ src/service/Servicios.java
@@ -3,7 +3,6 @@ package service;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import model.Camion;
 import model.Paquete;
 
@@ -24,17 +23,33 @@ public class Servicios {
         this.camiones = new ArrayList<>();
         this.paquetes = new ArrayList<>();
         this.paquetesPorCodigo = new HashMap<>();
+
+        //hardcode borrar
+        Paquete paquete1 = new Paquete(1, "A000", 10, true, 10);
+        this.agregarPaquete(paquete1);
     }
     /*
     * Expresar la complejidad temporal del servicio 1.
+    * busqueda por hashmap con clave
+    * Big O: O(n)
+    * Promedio: O(1)
     */
-    public Paquete servicio1(String codigoPaquete) { }
+    public Paquete servicio1(String codigoPaquete) { 
+            if (this.paquetesPorCodigo.containsKey(codigoPaquete)) {
+            return this.paquetesPorCodigo.get(codigoPaquete);
+        }
+        return null;
+    }
+    private void agregarPaquete(Paquete paquete) {
+        this.paquetes.add(paquete);
+        this.paquetesPorCodigo.put(paquete.getCodigoPaquete(), paquete);
+    }  
     /*
     * Expresar la complejidad temporal del servicio 2.
     */
-    public List<Paquete> servicio2(boolean contieneAlimentos) { }
+    public List<Paquete> servicio2(boolean contieneAlimentos) {return null;}
     /*
     * Expresar la complejidad temporal del servicio 3.
     */
-    public List<Paquete> servicio3(int urgenciaMinima, int urgenciaMaxima) { }
+    public List<Paquete> servicio3(int urgenciaMinima, int urgenciaMaxima) {return null;}
 }
