7cd7aae5899fc8475393d1e75468b155ac00dd0c
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-06-13 21:54:39 -0300

    fix csv primera linea no cantidad

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import service.Servicios;
 public class App {
     public static void main(String[] args) throws Exception {
         Servicios servicios = new Servicios("examples/Camiones.csv", "examples/Paquetes.csv");
-        System.out.println(servicios.servicio1("P001"));
-        System.out.println(servicios.servicio1("P999"));
+        System.out.println(servicios.servicio1("P001")); // Paquete [idPaquete=1, codigoPaquete=P001, peso=30, contieneAlimentos=true, nivelUrgencia=80]
+        System.out.println(servicios.servicio1("P999")); // null
     }
 }
