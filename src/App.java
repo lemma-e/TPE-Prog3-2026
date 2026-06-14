4484831c75ce6c572d190d7c1acd6d134fa8009c
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-06-13 22:28:10 -0300

    add tests

--- src/App.java
+++ src/App.java
@@ -3,7 +3,17 @@ import service.Servicios;
 public class App {
     public static void main(String[] args) throws Exception {
         Servicios servicios = new Servicios("examples/Camiones.csv", "examples/Paquetes.csv");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("servicio 1: ");
         System.out.println(servicios.servicio1("P001")); // Paquete [idPaquete=1, codigoPaquete=P001, peso=30, contieneAlimentos=true, nivelUrgencia=80]
         System.out.println(servicios.servicio1("P999")); // null
+
+
+        System.out.println("--------------------------------------------------------");
+        System.out.println("contiene alimentos: ");
+        System.out.println(servicios.servicio2(true));
+        System.out.println("&&&&&&&&&&&&&&&&&");
+        System.out.println("no contiene alimentos: ");
+    System.out.println(servicios.servicio2(false));
     }
 }
