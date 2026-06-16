56fc189392f8cc7b46341d6f16de300d4f4ef72f
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-06-16 20:13:25 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,32 +3,52 @@ import service.Servicios;
 public class App {
     public static void main(String[] args) throws Exception {
         Servicios servicios = new Servicios("examples/Camiones.csv", "examples/Paquetes.csv");
-        System.out.println("--------------------------------------------------------");
-        System.out.println("servicio 1: ");
-        System.out.println(servicios.servicio1("P001")); // Paquete [idPaquete=1, codigoPaquete=P001, peso=30, contieneAlimentos=true, nivelUrgencia=80]
-        System.out.println(servicios.servicio1("P999")); // null
-
 
         System.out.println("--------------------------------------------------------");
-        System.out.println("contiene alimentos: ");
+        System.out.println("SERVICIO 1:");
+        System.out.println("Complejidad promedio: O(1)");
+        System.out.println(" ");
+        System.out.println("Codigo P001:");
+        System.out.println(servicios.servicio1("P001"));
+        System.out.println(" ");
+        System.out.println("Codigo P999:");
+        System.out.println(servicios.servicio1("P999"));
+
+        System.out.println("--------------------------------------------------------");
+        System.out.println("SERVICIO 2:");
+        System.out.println("Complejidad: O(1)");
+        System.out.println(" ");
+        System.out.println("Paquetes con alimentos:");
         System.out.println(servicios.servicio2(true));
-        System.out.println("&&&&&&&&&&&&&&&&&");
-        System.out.println("no contiene alimentos: ");
+        System.out.println(" ");
+        System.out.println("Paquetes sin alimentos:");
         System.out.println(servicios.servicio2(false));
-        
-        
+
         System.out.println("--------------------------------------------------------");
-        System.out.println("50-100");
-        System.out.println(servicios.servicio3(50, 100));
-        System.out.println("1-10");
+        System.out.println("SERVICIO 3:");
+        System.out.println("Complejidad: O(N + M)");
+        System.out.println(" ");
+        System.out.println("Rango 1 a 10:");
         System.out.println(servicios.servicio3(1, 10));
-
+        System.out.println(" ");
+        System.out.println("Rango 50 a 100:");
+        System.out.println(servicios.servicio3(50, 100));
+        System.out.println(" ");
+        System.out.println("Rango 20 a 60:");
+        System.out.println(servicios.servicio3(20, 60));
 
         System.out.println("--------------------------------------------------------");
-        System.out.println("Backtracking:");
+        System.out.println("BACKTRACKING");
+        System.out.println("Complejidad: O((C + 1)^P)");
+        System.out.println("Metrica: estados generados");
+        System.out.println(" ");
         System.out.println(servicios.backtracking());
+
         System.out.println("--------------------------------------------------------");
-        System.out.println("Greedy:");
+        System.out.println("GREEDY");
+        System.out.println("Complejidad: O(P^2 + P*C)");
+        System.out.println("Metrica: candidatos considerados");
+        System.out.println(" ");
         System.out.println(servicios.greedy());
     }
 }
